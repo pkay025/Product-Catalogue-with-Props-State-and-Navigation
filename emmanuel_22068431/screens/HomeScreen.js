f1dba552dd5f1e56df4f11c1ba573b284cd9c807
pkay2151 <nunoomensahemmanuel224@gmail.com>
2026-08-09 21:07:40 +0000

    Refactor code structure for improved readability and maintainability

--- emmanuel_22068431/screens/HomeScreen.js
+++ emmanuel_22068431/screens/HomeScreen.js
@@ -4,51 +4,86 @@ import ProductCard from '../components/ProductCard';
 const products = [
   {
     id: '1',
-    image: '',
-    name: '',
+    image: 'AirPods Pro.png',
+    name: 'AirPods Pro',
     price: '$120',
     rating: '4.5',
   },
   {
     id: '2',
-    image: '',
-    name: '',
+    image: 'Nike Air Max 270.png',
+    name: 'Nike Air Max 270',
     price: '$90',
     rating: '4.3',
   },
   {
     id: '3',
-    image: '',
-    name: '',
+    image: 'Nike Air Force 1.png',
+    name: 'Nike Air Force 1',
     price: '$80',
     rating: '4.2',
   },
   {
     id: '4',
-    image: '',
-    name: '',
+    image: 'Women Perfume.png',
+    name: 'Women Perfume',
     price: '$110',
     rating: '4.7',
   },
   {
     id: '5',
-    image: '',
-    name: '',
+    image: 'Zealot Speaker.png',
+    name: 'Zealot Speaker',
     price: '$110',
     rating: '4.7',
   },
   {
     id: '6',
-    image: '',
-    name: '',
+    image: 'Premium Red Office dress.png',
+    name: 'Premium Red Office dress',
     price: '$110',
     rating: '4.7',
   },
 ];
 
 const HomeScreen = () => {
-  
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Products</Text>
+
+      <FlatList
+        data={products}
+        numColumns={2}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <ProductCard
+            image={item.image}
+            name={item.name}
+            price={item.price}
+            rating={item.rating}
+          />
+        )}
+      />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+    paddingTop: 20,
+  },
+
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginHorizontal: 16,
+    marginBottom: 10,
+  },
+});
+
+export default HomeScreen;
 
 
 
